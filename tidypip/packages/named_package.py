f7c925b9ed7b4eb9e553800f82246e848184552c
Chris Loew <cloew123@gmail.com>
2015-10-30 10:39:30 -0600

    The Named package can now return the text to be used in a requirements.txt file

--- tidypip/packages/named_package.py
+++ tidypip/packages/named_package.py
@@ -10,3 +10,7 @@ class NamedPackage:
     def installAs(self):
         """ Return the string to use to Install the package via pip """
         return self.name
+        
+    def forRequirements(self, versions):
+        """ Return the text to use for adding to the Requirements file """
+        return "{0}=={1}".format(versions.getProperName(self.name), versions[self.name])
