6faa5276ed53d751f8af57ef215ca5d0c17c1b83
Chris Loew <cloew123@gmail.com>
2015-10-30 09:26:45 -0600

    Added basic tidypip package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='tidypip',
+      version='0.1.0',
+      description="",
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      packages=['tidypip'],
+     )
