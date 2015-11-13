967f94e85ad5c728f4be2980621ebe70a4125c7d
Chris Loew <cloew123@gmail.com>
2015-11-13 08:56:56 -0700

    Fixed up Tidypip installation

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from distutils.core import setup
 
 setup(name='tidypip',
-      version='0.1.0',
+      version='0.1.1',
       description="",
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       packages=['tidypip',
+                'tidypip.args',
+                'tidypip.commands',
                 'tidypip.packages'],
-      install_requires=['kao_command']
+      install_requires=['kao_command'],
+      scripts=['tidypip/scripts/tidypip']
      )
